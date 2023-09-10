4ffe89c871fc12819be2c95c15dad0a89f0789cf
Ihechikara <ihechikara.dev@gmail.com>
2023-09-10 19:11:35 +0100

    back up PC files

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import express from "express"
+import express, { response } from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 
-import { Book } from "./models/bookModel"
+import { Book } from "./models/bookModel.js"
 
 dotenv.config()
 
@@ -10,8 +10,42 @@ const PORT = process.env.PORT
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
     res.json({msg: "Welcome"})
+})
+
+app.get("/api/books", async (req, res) => {
+    try {
+        const books = await Book.find({})
+        res.status(200).json(books)
+    } catch (error) {
+        console.log(error.message)
+        response.status(400).json({error: error.message})
+    }
+})
+
+
+app.post("/api/books", async (req, res) => {
+    const { title, author, pubYear } = req.body
+
+    if(!title || !author || !pubYear){
+        return res.status(400).send({
+            message: "Book info incomplete! Make sure you input title, author, and publish year"
+        })
+    }
+
+    try {
+
+        const book = await Book.create({title, author, pubYear})
+
+        return res.status(200).json(book)
+
+    } catch (error) {
+        console.log(error.message)
+        response.status(400).json({error: error.message})
+    }
 })
 
 mongoose
